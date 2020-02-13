9aa99b405428f2263884e7fa30b9c3e0434100b3
Lajos <lajos88.szoke@gmail.com>
2020-02-13 08:59:12 +0100

    settingname

--- js/section.js
+++ js/section.js
@@ -34,7 +34,7 @@ t.render(function () {
         var setting = settings[settingIndex];
         httpGet('v1/environments/' + setting.environmentId + '/settings/' + setting.settingId + '/value')
           .then(function (settingValue) {
-            var settingValueText = setting.name || '';
+            var settingValueText = setting.settingName || '';
 
             if ((!settingValue.rolloutRules || settingValue.rolloutRules.length === 0)
               && (!settingValue.percentageRules || settingValue.percentageRules.length === 0)) {

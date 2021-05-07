d1351a4fcfed5b5385df9af1408bec865b547189
Lajos <lajos88.szoke@gmail.com>
2021-05-07 12:01:19 +0200

    create and link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AddFeatureFlagComponent } from './add-feature-flag/add-feature-flag.component';
 import { FeatureFlagsSettingsComponent } from './feature-flags-settings/feature-flags-settings.component';
+import { CreateFeatureFlagComponent } from './create-feature-flag/create-feature-flag.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'authorize', component: AuthorizationComponent },
   { path: 'addfeatureflag', component: AddFeatureFlagComponent },
+  { path: 'createfeatureflag', component: CreateFeatureFlagComponent },
   { path: 'featureflags', component: FeatureFlagsSettingsComponent }
 ];
 
---
+++ src/app/create-feature-flag/create-feature-flag.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IntegrationLinkType, SettingType } from 'ng-configcat-publicapi';
+import { PublicApiService } from 'ng-configcat-publicapi-ui';
+import { Subscription } from 'rxjs';
+import { AuthorizationParameters } from '../models/authorization-parameters';
+import { TrelloService } from '../services/trello-service';
+
+@Component({
+  selector: 'app-create-feature-flag',
+  templateUrl: './create-feature-flag.component.html',
+  styleUrls: ['./create-feature-flag.component.scss']
+})
+export class CreateFeatureFlagComponent implements OnInit, OnDestroy {
+
+  formGroup: FormGroup;
+  authorizationParameters: AuthorizationParameters;
+  subscription: Subscription;
+  SettingTypeEnum = SettingType;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private trelloService: TrelloService,
+    private publicApiService: PublicApiService) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      productId: [null, [Validators.required]],
+      configId: [null, [Validators.required]],
+      environmentId: [null, [Validators.required]],
+      name: ['', [Validators.required, Validators.maxLength(255)]],
+      key: ['', [Validators.required, Validators.maxLength(255)]],
+      hint: ['', [Validators.maxLength(1000)]],
+      settingType: [SettingType.Boolean, [Validators.required]],
+    });
+
+    this.subscription = this.formGroup.statusChanges.subscribe(() => {
+      this.resize();
+    });
+
+    this.init();
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+      this.subscription = null;
+    }
+  }
+
+  async init() {
+    this.authorizationParameters = await this.trelloService.getAuthorizationParameters();
+    this.resize();
+  }
+
+  login(authorizationParameters) {
+    this.trelloService
+      .setAuthorizationParameters(authorizationParameters)
+      .then(() => {
+        this.init();
+      });
+  }
+
+  error() {
+    this.resize();
+  }
+
+  add() {
+    if (!this.formGroup.valid) {
+      return;
+    }
+
+    this.trelloService.getCardData()
+      .then(
+        card => {
+          return this.publicApiService
+            .createSettingsService(this.authorizationParameters.basicAuthUsername, this.authorizationParameters.basicAuthPassword)
+            .createSetting(this.formGroup.value.configId, {
+              key: this.formGroup.value.key,
+              settingType: this.formGroup.value.settingType,
+              name: this.formGroup.value.name,
+              hint: this.formGroup.value.hint
+            })
+            .toPromise()
+            .then(setting => {
+              return this.publicApiService
+                .createIntegrationLinksService(this.authorizationParameters.basicAuthUsername, this.authorizationParameters.basicAuthPassword)
+                .addOrUpdateIntegrationLink(this.formGroup.value.environmentId, setting.settingId,
+                  IntegrationLinkType.Trello, card.id,
+                  { description: card.name, url: card.url })
+                .toPromise();
+            });
+        }
+      )
+      .then(() => {
+        return this.trelloService.setCardSettingData({ lastUpdatedAt: new Date() });
+      })
+      .then(() => {
+        return this.trelloService.closePopup();
+      })
+      .catch(error => {
+        console.log(error);
+        // TODO
+      });
+  }
+
+  resize() {
+    setTimeout(() => {
+      this.trelloService.sizeTo('#outer');
+    }, 300);
+  }
+}

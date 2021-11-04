9c9f70309c9e29bedfafb0dd37abb336652b758b
Lajos <lali@configcat.com>
2021-11-04 12:37:22 +0100

    envsubst

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
-es5file=$(find /usr/share/nginx/html/main-es5*.js)
-variables=$(grep -o $es5file -e '\$\{CC_[a-zA-Z_0-9]*\}' -E | tr '\n' ' ')
-envsubst "$variables" < "$es5file" > /usr/share/nginx/html/tempes5.js
-mv /usr/share/nginx/html/tempes5.js "$es5file"
-es2015file=$(find /usr/share/nginx/html/main-es2015*.js)
-envsubst "$variables" < "$es2015file" > /usr/share/nginx/html/tempes2015.js
-mv /usr/share/nginx/html/tempes2015.js "$es2015file"
+jsfile=$(find /usr/share/nginx/html/main*.js)
+variables=$(grep -o $jsfile -e '\$\{CC_[a-zA-Z_0-9]*\}' -E | tr '\n' ' ')
+envsubst "$variables" < "$jsfile" > /usr/share/nginx/html/tempjsfile.js
+mv /usr/share/nginx/html/tempjsfile.js "$jsfile"
 nginx -g 'daemon off;'

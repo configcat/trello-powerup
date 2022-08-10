9fa25f4373fa62d36c398c97b233d953b48af06b
Lali <lali@configcat.com>
2022-08-10 19:34:15 +0200

    test

--- set-env.ts
+++ set-env.ts
@@ -1,6 +1,7 @@
 const { writeFile } = require ('fs');
 require('dotenv');
 const targetPath = './src/environments/environment.prod.ts';
+
 const envConfigFile = `export const environment = {
     production: true,
     publicApiBaseUrl: '${process.env.CC_PublicApiBaseUrl}',
